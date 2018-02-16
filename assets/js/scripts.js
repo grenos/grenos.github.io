9a1a0b1be4a64ce9df11a508e3fd7efdb3d761f2
grenos <Vasilis.green@gmail.com>
2018-02-16 19:55:58 +0200

    new cv link

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,10 +10,8 @@ jQuery(function ($) {
     });
 
     function getRandomClass() {
-        //Store available css classes
         var classes = new Array("pink", "yellow", "purple", "red", "blue", "orange", "teal", "lime", "green", "cyan", "indigo");
 
-        //Get a random number from 0 to 12
         var randomNumber = Math.floor(Math.random() * 12);
 
         return classes[randomNumber];
